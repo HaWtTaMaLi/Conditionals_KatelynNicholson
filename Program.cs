87314a00739b9bd2151960c5f5248cf6f312fd75
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-09-15 09:27:35 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditionals
+{
+    enum WeaponType
+    {
+        fist,
+        pistol,
+        rocketlauncher,
+        sniperRifle
+    }
+    internal class Program
+    {
+        static int weapon = 2;
+        static int enemyHealth = 100;
+        static WeaponType weapon = WeaponType ;        
+        static int berries = 50;
+        static int twigs = 40;
+        static int rocks = 30;
+
+        static void Main()
+        {
+            string 
+
+            //switch statment
+            switch(weapon)
+            {
+                case WeaponType.fist:
+                    //Fist 
+                    DealDamageToEnemy(20);
+                    break;
+                case WeaponType.pistol:
+                    // Pistol 
+                    DealDamageToEnemy(100);
+                    break;
+                case WeaponType.rocketlauncher:
+                    //Rocket 
+                    DealDamageToEnemy(100);
+                    break;
+                case WeaponType.sniperRifle:
+                    //Sniper 
+                    DealDamageToEnemy(50);
+                    break;
+                defult:
+                    Console.Write("weapon was not valid");
+
+            }
+        }
+
+        static void DealDamageToEnemy(int amount)
+        {
+            Console.WriteLine($"Enemy health was {enemyHealth}. They took {amount} damage."); // with $ you need {}
+            enemyHealth -= amount;
+            Console.WriteLine($"Enemy health is now {enemyHealth}.");
+        }
+    }
+}
